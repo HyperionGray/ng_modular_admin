2f4f05aa38be3b0209465e4888d4577a41569b4a
Mark E. Haase <mehaase@gmail.com>
2016-12-30 14:22:27 -0500

    Add link buttons.
    A link button is a button that is styled to resemble a link. I also added
    an 'href' property to <ma-button> that is a convenience to make a button
    navigate to a specified URL.

--- lib/components/button.dart
+++ lib/components/button.dart
@@ -12,25 +12,34 @@ class MaButton {
     @Input()
     bool block = false;
 
+    /// If set, a button click will trigger navigation
+    @Input()
+    String href;
+
     /// If true, the button is drawn as an outline with transparent background
     /// instead of a solid color. Default: false
     @Input()
     bool outline = false;
 
-    /// Button size: small, medium, or large. Default: medium.
-    @Input()
-    String size = 'medium';
-
     /// Round the corners of the left side or right side (or both).
     @Input()
     String pill;
+
+    /// Button size: small, medium, or large. Default: medium.
+    @Input()
+    String size = 'medium';
 
     /// The type of button (primary, secondary, success, info, warning, danger,
     /// or link). Default: primary
     @Input()
     String type = 'primary';
 
-    /// A click event emitter.
-    @Output()
-    final click = new EventEmitter<MouseEvent>();
+    /// Handle navigation to an href, if an href is set.
+    @HostListener('click', const [r'$event'])
+    void onClick(MouseEvent event) {
+        if (this.href != null) {
+            window.location.replace(this.href);
+            event.stopPropagation();
+        }
+    }
 }
